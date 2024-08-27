8dbe314156228aea6594e678e3802a546a1814de
HamGNX <104154109+HamGNX@users.noreply.github.com>
2024-08-28 01:00:43 +0700

    Q3

--- Lab1Hw/Q3/Program.cs
+++ Lab1Hw/Q3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Transactions;
 
 namespace Q3
 {
@@ -7,11 +6,38 @@ namespace Q3
     {
         static void Main(string[] args)
         {
-        // input number
-        Console.WriteLine("Enter the number of elements in the array: ");
-        int n = Convert.ToInt32(Console.ReadLine());
+            // ask user
+            Console.WriteLine("Enter the number of elements in the array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-        
+            // array
+            int[] array = new int[n];
+
+            // get elements 
+            Console.WriteLine("Enter " + n + " integer elements:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Element " + (i + 1) + ": ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Calculate sum, max, min, and average
+            int sum = 0;
+            int max = array[0];
+            int min = array[0];
+            for (int i = 0; i < n; i++)
+            {
+                sum += array[i];
+                if (array[i] > max) max = array[i];
+                if (array[i] < min) min = array[i];
+            }
+            double average = (double)sum / n;
+
+            // Print results
+            Console.WriteLine("Sum of elements: " + sum);
+            Console.WriteLine("Maximum element: " + max);
+            Console.WriteLine("Minimum element: " + min);
+            Console.WriteLine("Average of elements: " + average);
         }
     }
 }
--- Lab1Hw/Q3/obj/Debug/net8.0/Q3.AssemblyInfo.cs
+++ Lab1Hw/Q3/obj/Debug/net8.0/Q3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Q3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d88308b84dae930034daf12208de9629ef23b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597e6c3508703c7f819021bd56f34e9cc4549451")]
 [assembly: System.Reflection.AssemblyProductAttribute("Q3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Q3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
