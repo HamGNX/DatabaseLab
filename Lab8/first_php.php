3b6cead009c519becb9a92369d3c67fb38218e9c
HamGNX <104154109+HamGNX@users.noreply.github.com>
2024-10-18 10:35:49 +0700

    doinglab8

--- Lab8/first_php.php
+++ Lab8/first_php.php
@@ -1,3 +1,14 @@
-<?php 
-    echo "Hello World!";
-?>;
+<!DOCTYPE html>
+<html>
+<head>
+<title>Basic PHP</title>
+</head>
+<body>
+<?php
+echo "<h1>Hello, World</h1>";
+echo "10 + 20 = ";
+echo 10+20;
+echo "\n";
+?>
+</body>
+</html>
